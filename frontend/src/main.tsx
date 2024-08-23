1467c60d952f9d2e628405b70f7aebbfddd02eea
Adnan <s.adnan@aabtools.com>
2024-08-23 17:25:03 +0400

    FE: Added react router and base css for MUI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import App from './App.tsx'
 
 import { darkTheme } from './themes/defaultTheme.ts'
-import CssBaseline from '@mui/material/CssBaseline'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 )
