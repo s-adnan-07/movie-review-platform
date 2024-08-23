3ee75d25560f6a508253551bb62ef541f68b9a51
Adnan <s.adnan@aabtools.com>
2024-08-23 13:21:10 +0400

    BE: Updated local auth strategy to display appropriate error message

--- backend/src/apps/auth/strategies/local.strategy.ts
+++ backend/src/apps/auth/strategies/local.strategy.ts
@@ -12,6 +12,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser({ email, password })
     if (user) return user
-    throw new UnauthorizedException()
+    throw new UnauthorizedException('Invalid email or password')
   }
 }
