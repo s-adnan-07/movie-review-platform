3dabfe8481d20d90f523a744d8ace61d9883b596
Adnan <s.adnan@aabtools.com>
2024-08-23 13:25:11 +0400

    BE: Updated email and password requirements for user registration
    Made the requirements more strict. Email should be a valid email address. Password should be a minimum of 8 characters long.

--- backend/src/shared/dtos/login-details.dto.ts
+++ backend/src/shared/dtos/login-details.dto.ts
@@ -1,11 +1,12 @@
-import { IsNotEmpty, IsString } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
 
 export class LoginDetailsDto {
   @IsNotEmpty()
-  @IsString()
+  @IsEmail()
   email: string
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(8)
   password: string
 }
