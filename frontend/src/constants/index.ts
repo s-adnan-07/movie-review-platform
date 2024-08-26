683fc732be8a9c6cd7a21424efe157cfb0b80281
Adnan <s.adnan@aabtools.com>
2024-08-26 14:09:32 +0400

    FE: Added default server url

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,5 @@
-export const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL
+export const VITE_SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || 'http://localhost:4040/api/v1'
 // export const VITE_SERVER_URL = 'http://localhost:3000/api/v1'
 export const GENRES = ['action', 'comedy', 'thriller', 'drama', 'horror']
 export const ONE_SECOND = 1000
