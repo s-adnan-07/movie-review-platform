fa9eaf64591ef0be58777bd8784e8ff099e21c02
Adnan <s.adnan@aabtools.com>
2024-08-26 13:24:00 +0400

    FE: Updated navbar based on user auth value

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,47 @@
+import { Link } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
+import Button from '@mui/material/Button'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
+
+import { useAuth } from '@/contexts/AuthContext'
+import useLogout from '@/hooks/useLogout'
+import Box from '@mui/material/Box'
 
 function Navbar() {
+  const { user } = useAuth()
+  const { handleLogout } = useLogout()
+
   return (
-    <AppBar position="sticky">
-      <Toolbar>
-        <Typography variant="h5" fontWeight={700}>
-          Home
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky">
+        <Toolbar variant="dense">
+          <Box sx={{ flexGrow: 1 }}>
+            <Button component={Link} to={'/'} variant="text">
+              Home
+            </Button>
+            {user?.role == 'admin' && (
+              <Button component={Link} to={'/add-movie'} variant="text">
+                Add Movie
+              </Button>
+            )}
+            {user && (
+              <Button component={Link} to={'/my-reviews'} variant="text">
+                My Reviews
+              </Button>
+            )}
+          </Box>
+          {user ? (
+            <Button onClick={handleLogout} variant="text">
+              Logout
+            </Button>
+          ) : (
+            <Button component={Link} to={`/login`} variant="text">
+              Login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
