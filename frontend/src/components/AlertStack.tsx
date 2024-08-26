39ab5455b7a083d2dc3159ebe0914bee1c022a55
Adnan <s.adnan@aabtools.com>
2024-08-26 13:24:45 +0400

    FE: Added key to alert messages

--- frontend/src/components/AlertStack.tsx
+++ frontend/src/components/AlertStack.tsx
@@ -12,8 +12,8 @@ function AlertStack({ messages, severity = 'error' }: Props) {
   return (
     <Collapse in={Boolean(messages.length)}>
       <Stack>
-        {messages.map(message => (
-          <Alert variant="filled" severity={severity}>
+        {messages.map((message, i) => (
+          <Alert variant="filled" severity={severity} key={i}>
             {message}
           </Alert>
         ))}
